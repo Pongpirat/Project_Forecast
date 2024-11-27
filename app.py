06576d2a6ae27e4580557600bca0c770e34f5d65
Pongpirat <pong.pirat203@gmail.com>
2024-11-28 00:41:39 +0700

    test app.py fix web

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import plotly.graph_objects as go
 
 from data_processing import process_data
